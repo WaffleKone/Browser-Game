13d2a4ae93cfd8e32fb12ec90f900bf46fa0935c
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-05-04 18:15:16 -0400

    renamed variable in showIntro to make sense

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ function hideMenu() {
 }
 // function to display the transition sequence the player sees before the game begins, explaining the object of the game. will automatically lead to the game portion.
 function showIntro() {
-  let newElement = document.createElement('div');
-  newElement.setAttribute('id', 'intro');
-  document.body.appendChild(newElement);
+  let newDiv = document.createElement('div');
+  newDiv.setAttribute('id', 'intro');
+  document.body.appendChild(newDiv);
   let newH3 = document.createElement('h3');
   newH3.className += 'fadein';
   newH3.innerHTML =
     'You will be shown a word chosen at random, you must choose if you have seen the word during this experiment or if it is a new word in the experiment. Choosing incorrectly ends the experiment.';
-  newElement.append(newH3);
+  newDiv.append(newH3);
   setTimeout(() => {
     newH3.className = 'fadeout';
   }, 5000); // have the rules fade out after 5 seconds (ms)
   setTimeout(() => {
-    newElement.setAttribute('hidden', 'true')
+    newDiv.setAttribute('hidden', 'true')
     createGame();
   }, 2000); // hides the intro div after 2 seconds (equal to the animation duration of fadeout in the CSS file.), then calls the game to be created
 }
